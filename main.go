8ecf1c92f7f1ea5e8e9ab7643f8c1a731aa1bf8b
nagiyevdt <derya.nagiyev@kapitalbank.az>
2025-02-24 18:58:25 +0400

    added new API to project
    create account
    getaccount by id

--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
-//TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
+import (
+	"database/sql"
+	_ "github.com/jackc/pgx/v5/pgxpool"
+	_ "github.com/lib/pq"
+	"log"
+	"simple-banking/api"
+	db "simple-banking/db/sqlc"
+)
+
+const (
+	dbdriver       = "postgres"
+	dataSourceName = "postgresql://root:root@localhost:5432/simple_bank?sslmode=disable"
+	Serveraddress  = "localhost:7070"
+)
 
 func main() {
+	conn, err := sql.Open(dbdriver, dataSourceName)
+
+	if err != nil {
+		log.Fatal("can not connect to db", err)
+	}
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(Serveraddress)
+	if err != nil {
+		log.Fatal("Server couldn't started", err)
+	}
 
 }
-
-//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
-// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
